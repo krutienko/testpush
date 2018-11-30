000ea09e5d6d94e069c37f21eaaa8c00a9fcad23
Pavel <krutienko@users.noreply.github.com>
2018-11-30 13:00:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
-
+if ('serviceWorker' in navigator) {
+  // Регистрация service worker-а, расположенного в корне сайта
+  // за счет использования дефолтного scope (не указывая его)
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker зарегистрирован:', registration);
+  }).catch(function(error) {
+    console.log('Ошибка при регистрации service worker-а:', error);
+  });
+} else {
+  // Текущий браузер не поддерживает service worker-ы.
+  console.log('Текущий браузер не поддерживает service worker-ы');
+}
 
 // firebase.initializeApp({
 //     messagingSenderId: '87827361533'
