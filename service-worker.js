f0d3147d1c758dc8c03818666826774ae2ac80f2
Krutienko Pavel <82.pavel@gmail.com>
2018-12-04 15:27:27 +0200

    simple push test

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+this.onpush = function(event) {
+	console.log(event.data);
+	// From here we can write the data to IndexedDB, send it to any open
+	// windows, display a notification, etc.
+}
+
+
+// // Register event listener for the 'push' event.
+// self.addEventListener('push', function(event) {
+//   // Keep the service worker alive until the notification is created.
+//   event.waitUntil(
+//     // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
+//     self.registration.showNotification('ServiceWorker Cookbook', {
+//       body: 'Alea iacta est',
+//     })
+//   );
+// });
